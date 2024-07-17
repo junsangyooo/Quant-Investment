8355ce796d9838313a7eabfd616bb25a2b614360
Jun Yoo <70479629+junsangyooo@users.noreply.github.com>
2024-07-17 17:22:57 +0900

    main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from data_analysis import analyze_data
 from stock_selection import select_stocks
 from trade_execution import execute_trade
 
+# Basic instruction: https://wikidocs.net/164056
+# 계좌번호: 64428904
+# APP Key: PS87iiWlksmzSU8JilwwZyO6I6pH4k4qKXeR
+# APP Secret:E9mgaarv+ndZ93+LU2+bKjYWAXHanH8+g0vtDLCYVN6sAKX81YQNJAO3SajR74nzboVgPu1NA2jQkGJai9pCAe/8gTEGTM1L58udnrRQUpAZTP9Jes3d3X06a3iBsO6RF0EA2qyQZtmvWuesHnO7Gl653+wcFZAIw+mydJTFQAp1pI3Vgbw=
+
 # def main():
 #     # Analyze stock data
 #     data = analyze_data()
