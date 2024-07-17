13ea589f0e846c3130099df69763cab0f4727708
Jun Yoo <70479629+junsangyooo@users.noreply.github.com>
2024-07-17 17:24:00 +0900

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from data_analysis import analyze_data
 from stock_selection import select_stocks
 from trade_execution import execute_trade
 
-# Basic instruction: https://wikidocs.net/164056
+# Basic instruction(blog): https://wikidocs.net/164056
+# Basic instruction(api): https://apiportal.koreainvestment.com/apiservice/oauth2#L_5c87ba63-740a-4166-93ac-803510bb9c02
 # 계좌번호: 64428904
 # APP Key: PS87iiWlksmzSU8JilwwZyO6I6pH4k4qKXeR
 # APP Secret:E9mgaarv+ndZ93+LU2+bKjYWAXHanH8+g0vtDLCYVN6sAKX81YQNJAO3SajR74nzboVgPu1NA2jQkGJai9pCAe/8gTEGTM1L58udnrRQUpAZTP9Jes3d3X06a3iBsO6RF0EA2qyQZtmvWuesHnO7Gl653+wcFZAIw+mydJTFQAp1pI3Vgbw=
