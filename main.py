b633a8d5af246477b31be85b405126d3ad2d2b27
Jun Yoo <70479629+junsangyooo@users.noreply.github.com>
2024-07-19 11:13:04 +0000

    main.py

--- main.py
+++ main.py
@@ -16,10 +16,12 @@ key_bytes = 32
 
 # Basic instruction(blog): https://wikidocs.net/profile/info/book/13688
 # Basic instruction(api): https://apiportal.koreainvestment.com/apiservice/oauth2#L_5c87ba63-740a-4166-93ac-803510bb9c02
+# KoreaInvest Github: https://github.com/koreainvestment/open-trading-api
+# Jocoding Github: https://github.com/youtube-jocoding/koreainvestment-autotrade
 # postman.com/
 # 계좌번호: 64428904
-appKey = "PS87iiWlksmzSU8JilwwZyO6I6pH4k4qKXeR"
-appSecret = "E9mgaarv+ndZ93+LU2+bKjYWAXHanH8+g0vtDLCYVN6sAKX81YQNJAO3SajR74nzboVgPu1NA2jQkGJai9pCAe/8gTEGTM1L58udnrRQUpAZTP9Jes3d3X06a3iBsO6RF0EA2qyQZtmvWuesHnO7Gl653+wcFZAIw+mydJTFQAp1pI3Vgbw="
+APP_KEY = "PS87iiWlksmzSU8JilwwZyO6I6pH4k4qKXeR"
+APP_SECRET = "E9mgaarv+ndZ93+LU2+bKjYWAXHanH8+g0vtDLCYVN6sAKX81YQNJAO3SajR74nzboVgPu1NA2jQkGJai9pCAe/8gTEGTM1L58udnrRQUpAZTP9Jes3d3X06a3iBsO6RF0EA2qyQZtmvWuesHnO7Gl653+wcFZAIw+mydJTFQAp1pI3Vgbw="
 
 # AES256 DECODE
 def aes_cbc_base64_dec(key, iv, cipher_text):
