196e93d01250718ae4a3795913d83330723e5276
Jun Yoo <70479629+junsangyooo@users.noreply.github.com>
2024-07-19 09:41:02 +0000

    main.py

--- main.py
+++ main.py
@@ -5,18 +5,5 @@ from trade_execution import execute_trade
 # Basic instruction(blog): https://wikidocs.net/164056
 # Basic instruction(api): https://apiportal.koreainvestment.com/apiservice/oauth2#L_5c87ba63-740a-4166-93ac-803510bb9c02
 # 계좌번호: 64428904
-# APP Key: PS87iiWlksmzSU8JilwwZyO6I6pH4k4qKXeR
-# APP Secret:E9mgaarv+ndZ93+LU2+bKjYWAXHanH8+g0vtDLCYVN6sAKX81YQNJAO3SajR74nzboVgPu1NA2jQkGJai9pCAe/8gTEGTM1L58udnrRQUpAZTP9Jes3d3X06a3iBsO6RF0EA2qyQZtmvWuesHnO7Gl653+wcFZAIw+mydJTFQAp1pI3Vgbw=
-
-# def main():
-#     # Analyze stock data
-#     data = analyze_data()
-
-#     # Select stocks based on analysis
-#     selected_stocks = select_stocks(data)
-
-#     # Execute trades on selected stocks
-#     execute_trade(selected_stocks)
-
-# if __name__ == "__main__":
-#     main()
+appKey = "PS87iiWlksmzSU8JilwwZyO6I6pH4k4qKXeR"
+appSecret = "E9mgaarv+ndZ93+LU2+bKjYWAXHanH8+g0vtDLCYVN6sAKX81YQNJAO3SajR74nzboVgPu1NA2jQkGJai9pCAe/8gTEGTM1L58udnrRQUpAZTP9Jes3d3X06a3iBsO6RF0EA2qyQZtmvWuesHnO7Gl653+wcFZAIw+mydJTFQAp1pI3Vgbw="
