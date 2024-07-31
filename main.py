9a8140a91571fd484b259ddd66eaa253ce029e84
Jun Yoo <70479629+junsangyooo@users.noreply.github.com>
2024-07-31 11:12:34 +0000

    commit main

--- main.py
+++ main.py
@@ -23,13 +23,28 @@ key_bytes = 32
 APP_KEY = "PS87iiWlksmzSU8JilwwZyO6I6pH4k4qKXeR"
 APP_SECRET = "E9mgaarv+ndZ93+LU2+bKjYWAXHanH8+g0vtDLCYVN6sAKX81YQNJAO3SajR74nzboVgPu1NA2jQkGJai9pCAe/8gTEGTM1L58udnrRQUpAZTP9Jes3d3X06a3iBsO6RF0EA2qyQZtmvWuesHnO7Gl653+wcFZAIw+mydJTFQAp1pI3Vgbw="
 
-# AES256 DECODE
-def aes_cbc_base64_dec(key, iv, cipher_text):
-    """
-    :param key:  str type AES256 secret key value
-    :param iv: str type AES256 Initialize Vector
-    :param cipher_text: Base64 encoded AES256 str
-    :return: Base64-AES256 decodec str
-    """
-    cipher = AES.new(key.encode('utf-8'), AES.MODE_CBC, iv.encode('utf-8'))
-    return bytes.decode(unpad(cipher.decrypt(b64decode(cipher_text)), AES.block_size))
+headers = {"content-type":"application/json"}
+body = {"grant_type":"client_credentials",
+        "appkey":APP_KEY, 
+        "appsecret":APP_SECRET}
+
+URL_BASE = "https://openapivts.koreainvestment.com:29443" #모의투자서비스
+PATH = "oauth2/tokenP"
+URL = f"{URL_BASE}/{PATH}"
+
+res = requests.post(URL, headers=headers, data=json.dumps(body))
+
+ACCESS_TOKEN = res.json()["access_token"]
+print(URL)
+print(ACCESS_TOKEN)
+
+# # AES256 DECODE
+# def aes_cbc_base64_dec(key, iv, cipher_text):
+#     """
+#     :param key:  str type AES256 secret key value
+#     :param iv: str type AES256 Initialize Vector
+#     :param cipher_text: Base64 encoded AES256 str
+#     :return: Base64-AES256 decodec str
+#     """
+#     cipher = AES.new(key.encode('utf-8'), AES.MODE_CBC, iv.encode('utf-8'))
+#     return bytes.decode(unpad(cipher.decrypt(b64decode(cipher_text)), AES.block_size))
