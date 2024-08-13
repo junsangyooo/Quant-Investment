120c18437c13bbcc069408726d5681839bb30bb4
Jun Yoo <70479629+junsangyooo@users.noreply.github.com>
2024-08-13 09:48:08 +0000

    commit scrap_historical_data.py

--- scrap_historical_data.py
+++ scrap_historical_data.py
@@ -5,7 +5,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from io import StringIO
 import pandas as pd
 import time
 import os
@@ -62,17 +61,15 @@ def getStock(indList = None):
             stocks.append(stock(sym,name,industry))
     return stocks
 
-def getHistoricalData(st):
-    url = st.getUrl()
-    sym = st.getSym()
-
+def getHistoricalData(stocks):
     # Set up the directory where the script is running as the download directory
     current_directory = os.getcwd()
+    download_directory = os.path.join(current_directory, 'historical_data')
 
     # Selenium Setting
     chrome_options = Options()
     prefs = {
-        "download.default_directory": current_directory,  # Set the download directory to current directory
+        "download.default_directory": download_directory,  # Set the download directory to current directory
         "download.prompt_for_download": False,
         "directory_upgrade": True,
         "safebrowsing.enabled": True
@@ -82,45 +79,41 @@ def getHistoricalData(st):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-# Initialize WebDriver with the Chrome options
+    # Initialize WebDriver with the Chrome options
     driver = webdriver.Chrome(options=chrome_options)
-    #driver = webdriver.Chrome()
-    driver.get(url)
-    
-    # Wait for the "Time Period" button to be clickable and click it
-    time_period_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CLASS_NAME, "tertiary-btn.fin-size-small.menuBtn.rounded.yf-122t2xs"))
-    )
-    time_period_button.click()
+    for st in stocks:
+        url = st.getUrl()
+        #driver = webdriver.Chrome()
+        driver.get(url)
+        
+        # Wait for the "Time Period" button to be clickable and click it
+        time_period_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "tertiary-btn.fin-size-small.menuBtn.rounded.yf-122t2xs"))
+        )
+        time_period_button.click()
 
-    # Wait for the menu to appear and the "Max" button to be clickable
-    max_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[text()='Max']"))
-    )
-    max_button.click()
+        # Wait for the menu to appear and the "Max" button to be clickable
+        max_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[text()='Max']"))
+        )
+        max_button.click()
 
-    # Wait for the download button to be clickable and click it
-    download_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//a[@data-ylk='elm:download;elmt:link;itc:1;sec:qsp-historical;slk:history-download;subsec:download']"))
-    )
-    download_button.click()
+        # Wait for the download button to be clickable and click it
+        download_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//a[@data-ylk='elm:download;elmt:link;itc:1;sec:qsp-historical;slk:history-download;subsec:download']"))
+        )
+        download_button.click()
 
-    # Wait for the file to be downloaded
-    time.sleep(10)
+        # Wait for the file to be downloaded
+        time.sleep(10)
 
     # Close the browser
     driver.quit()
 
-    #st.setHistory(pd.read_csv(f'{sym}.csv'))
+def main():
+    # We are going to use only technology stocks for the prototype
+    indList = ['Technology']
+    stocks = getStock(indList)
+    getHistoricalData(stocks)
 
-# def getDF(stocks):
-#     for st in stocks: getHistoricalData(st)
-    
-
-
-indList = ['Technology']
-stocks = getStock(indList)
-getHistoricalData(stocks[0])
-
-df = stocks[0].getHistory()
-print(df)
+main()
