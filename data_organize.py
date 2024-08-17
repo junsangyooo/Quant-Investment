850c7ba5966936aa075003b35d86dc9ad2378e30
Jun Yoo <70479629+junsangyooo@users.noreply.github.com>
2024-08-17 07:57:44 +0000

    modified data_organize.py

--- data_organize.py
+++ data_organize.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 current_directory = os.getcwd()
-csv_directory = os.path.join(current_directory, 'historical_data')
+csv_directory = os.path.join(current_directory, 'stock_data')
 def convert_csv_to_df():
     df = pd.read_csv(os.path.join(csv_directory, 'AAPL.csv'))
     #df.set_index('Date', inplace=True)
